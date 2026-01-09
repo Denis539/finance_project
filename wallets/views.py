347c36ef1dd8236e1139458e925008da41e5a894
Denis Komornikov <komornikovda@mail.ru>
2026-01-09 19:46:58 +0500

    hosting health check

--- wallets/views.py
+++ wallets/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>SmartSave MVP</h1><p>Система интеллектуального планирования накоплений запущена!</p>")
+    return HttpResponse("<h1>MVP</h1><p>Система интеллектуального планирования накоплений запущена!</p><p>Проверка изменений</p>")
