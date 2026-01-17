145f464e1a6546f221d517d23e352ff8a4bea87a
Denis Komornikov <komornikovda@mail.ru>
2026-01-17 22:48:31 +0500

    Successfully

--- wallets/views.py
+++ wallets/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from .models import FinancialGoal, Transaction
-from django.contrib.auth.models import User
 
 def index(request):
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'admin')
-
     goals_count = FinancialGoal.objects.count()
     trans_count = Transaction.objects.count()
     
