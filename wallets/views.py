27459073d1e9514a8108ad49b1684cb4e6f52f6e
Denis Komornikov <komornikovda@mail.ru>
2026-01-18 15:17:45 +0500

    .

--- wallets/views.py
+++ wallets/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import FinancialGoal, Transaction
+from django.contrib.auth.models import User
+
+
 
 def index(request):
     goals_count = FinancialGoal.objects.count()
     trans_count = Transaction.objects.count()
+    # Внутри функции index(request) перед return:
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin')
     
     context = {
         'goals_count': goals_count,
