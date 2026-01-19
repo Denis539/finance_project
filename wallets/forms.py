0b82ded3f7ee33934f8dcae444e31eb2232e1b08
Denis Komornikov <komornikovda@mail.ru>
2026-01-20 01:03:37 +0500

    Final version

--- wallets/forms.py
+++ wallets/forms.py
@@ -1,16 +1,25 @@
 from django import forms
 from .models import Transaction
 from .models import Category, FinancialGoal
-
+from django.db.models import Q
 
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = ['category', 'amount']
         widgets = {
-            'category': forms.Select(attrs={'class': 'w-full border-2 border-black p-2', 'placeholder': 'Например: Стипендия'}),
+            'category': forms.Select(attrs={'class': 'w-full border-2 border-black p-2'}),
             'amount': forms.NumberInput(attrs={'class': 'w-full border-2 border-black p-2', 'placeholder': '0.00'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super(TransactionForm, self).__init__(*args, **kwargs)
+        
+        if user:
+            self.fields['category'].queryset = Category.objects.filter(
+                Q(user=user) | Q(user__isnull=True)
+            ).order_by('name')
         
 class CategoryForm(forms.ModelForm):
     class Meta:
